8d1f5294d25d46b98438b924f1b46965ad958469
Richard Song <brsong22@gmail.com>
2025-05-23 15:10:00 -0700

    updating linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,37 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                project: "./tsconfig.json",
+                tsconfigRootDir: __dirname,
+            },
+        },
+        plugins: {
+            "@typescript-eslint": tsPlugin,
+        },
+        rules: {
+            indent: ["error", 4], // disable base rule
+            //   "@typescript-eslint/indent": ["error", 4],
+            semi: ["error", "always"],
+            quotes: ["error", "double"],
+        },
+    },
 ];
 
 export default eslintConfig;
